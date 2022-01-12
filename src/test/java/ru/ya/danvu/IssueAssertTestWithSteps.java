19744d0e53a6926c70412e8096ba1a515764896d
danvu <danvu@ya.ru>
2022-01-12 23:24:58 +0300

    Тесты с разными подходами

---
+++ src/test/java/ru/ya/danvu/IssueAssertTestWithSteps.java
@@ -0,0 +1,49 @@
+package ru.ya.danvu;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
+import static org.openqa.selenium.By.linkText;
+import static org.openqa.selenium.By.partialLinkText;
+
+public class IssueAssertTestWithSteps {
+
+    private static final String REPOSITORY = "eroshenkoam/allure-example";
+    private static final int NUMBER = 68;
+
+    @Test
+    public void lambdaStepTest() {
+        step("Открываем гитхаб", () -> {
+           open("https://github.com");
+        });
+        step("Ищем репозиторий " + REPOSITORY, () -> {
+            $(".header-search-input").click();
+            $(".header-search-input").sendKeys(REPOSITORY);
+            $(".header-search-input").submit();
+        });
+        step("Переходим в репозиторий " + REPOSITORY, () -> {
+            $(linkText(REPOSITORY)).click();
+        });
+        step("Открываем таб Issues ", () -> {
+            $(partialLinkText("Issues")).click();
+        }) ;
+        step("Проверяем наличие Issue с номером " + NUMBER, () -> {
+            $(withText("#" + NUMBER)).should(Condition.visible);
+        });
+    }
+
+    @Test
+    public void annotatedStepsTest() {
+     IssueAssertTestWithWebSteps steps = new IssueAssertTestWithWebSteps();
+
+     steps.openMainPage();
+        steps.searchForRepository(REPOSITORY);
+        steps.openRepositoryPage(REPOSITORY);
+        steps.openIssuesTab();
+        steps.shouldSeeIssueWithNumber(NUMBER);
+    }
+}
